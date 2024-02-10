c2468c2ce6dfa57a50170058c86a90e1fb842352
gacto <gebyzayumba@gmail.com>
2024-02-10 14:47:12 +0300

    add image to gatsby website by using local images

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
       <p>Welcome to Home page</p>
       <StaticImage
       alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-      src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      src="../images/pic.jpg"
       />
     </Layout>
   </main>
